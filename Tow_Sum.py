f42e0f0e5ec570718b906925e430999fbe415196
zpf <365078225@qq.com>
2018-08-09 20:49:30 +0800

    version 2 done

--- Tow_Sum.py
+++ Tow_Sum.py
@@ -19,6 +19,12 @@ class Solution(object):
         # return res
 
         # version 2 hash table
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
 
 
 if __name__ == "__main__":
