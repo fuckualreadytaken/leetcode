27a13187821c7d22863def6d39f5c0d2b313cd91
zpf <365078225@qq.com>
2018-08-09 11:31:52 +0800

    Initial commit

---
+++ Tow_Sum.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*-coding:utf-8-*-
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # version 1 brute force
+        # for (index1, n1) in enumerate(nums):
+        #     index1 += 1
+        #
+        #     for (index2, n2) in enumerate(nums[index1:]):
+        #         index2 += index1
+        #         if n1 + n2 == target:
+        #             res = [index1 - 1, index2]
+        #
+        # return res
+
+        # version 2 hash table
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.twoSum([2, 7, 11, 15], 9))
